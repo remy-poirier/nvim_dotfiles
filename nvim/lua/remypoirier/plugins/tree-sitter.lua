b6e5006be1fda39170c2021a8a05135093c3a50f
Remy-Poirier <remy.poirier92@gmail.com>
2023-11-06 15:30:46 +0100

    Add lsp configuration files

---
+++ nvim/lua/remypoirier/plugins/tree-sitter.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function ()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "javascript", "html", "css", "csv", "graphql", "java", "javascript", "json", "json5", "prisma", "scala", "sql", "tsx", "typescript", "xml", "yaml" },
+			indent = { enable = true },
+			highlight = { enable = true },
+			sync_install = false,
+			auto_install = true,
+			additional_vim_regex_highlighting = false,
+		})
+	end
+}
