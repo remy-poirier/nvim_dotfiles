a180decb552568aba86d64c9e0f97a2b4197f666
Remy-Poirier <remy.poirier92@gmail.com>
2023-11-10 17:27:02 +0100

    Almost done with config

---
+++ nvim/after/plugin/netrw-icons.lua
@@ -0,0 +1,8 @@
+require'netrw'.setup{
+  -- Put your configuration here, or leave the object empty to take the default
+  -- configuration.
+  use_devicons = true,
+  icons = {
+    file = "f",
+  }
+}
