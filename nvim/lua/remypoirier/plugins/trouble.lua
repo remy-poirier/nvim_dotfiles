9cdf86985bc4335f7460605e64c6d47e04311c18
Remy-Poirier <remy.poirier92@gmail.com>
2024-05-07 16:52:48 +0200

    Update lsp configuration + add 2 new themes

--- nvim/lua/remypoirier/plugins/trouble.lua
+++ nvim/lua/remypoirier/plugins/trouble.lua
@@ -1,9 +1,11 @@
-return { "folke/trouble.nvim",
+return {
+  "folke/trouble.nvim",
+
   branch = "dev", -- IMPORTANT!
   keys = {
     {
-      "<leader>tt",
-      "<cmd>Trouble diagnostics toggle<cr>",
+      "<leader>gt",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
       desc = "Diagnostics (Trouble)",
     },
     {
@@ -13,9 +15,11 @@ return { "folke/trouble.nvim",
     },
     {
       "<leader>tp",
-      "<cmd>Trouble diagnostics prev skip_groups=true jump=true<cr>",
+      "<cmd>Trouble diagnostics prev skip_groups=true jump=tru<cr>",
       desc = "Previous diagnostic (Trouble)",
-    }
+    },
   },
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    auto_close = true, -- This should close the trouble window when items are empty
+  }, -- for default options, refer to the configuration section for custom setup.
 }
